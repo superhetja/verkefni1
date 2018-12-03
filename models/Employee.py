0276f102e6e8bbf108c995f353f79219a289e34c
sara <sarae17@ru.is>
2018-12-03 13:39:27 +0000

    sara saeta

--- models/Employee.py
+++ models/Employee.py
@@ -7,5 +7,19 @@ class Employee:
         self.__email = email
         self.__admin = admin
         self.__ssn = ssn
-
     
+    def get_fullname(self):
+        return self.__fullname
+
+    def get_password(self):
+        return self.__username
+
+    def get_email(self):
+        return self.__email
+
+    def get_admin(self):
+        return self.__admin
+
+    def get_ssn(self):
+        return self.__ssn
+
