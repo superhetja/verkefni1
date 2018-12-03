4322e3e03ad1e8a492801510fba3dbb8b33a9b39
Bryndís Diego <bryndisdiego@Bryndiss-MacBook.local>
2018-12-03 12:09:10 +0000

    'bryndis'

--- models/van.py
+++ models/van.py
@@ -1,12 +1,21 @@
 from car import Car 
 
-class van:
-    def __init__(self, price=0):
-        self.__price = price()
+class van(Car):
+    PRICE_PER_DAY = 9000
+    def __init__(self, brand='', days=1, seats=0, transmission='', doors=0):
+        Car.__init__(self, brand, days, seats, transmission, doors)
+        self.__price = self.PRICE_PER_DAY * days
+
+
 
     def set_price(self):
-        '''Endurreiknar verð á Van'''
         return self.__price
 
     def get_price(self):
         return self.__price.get_price()
+
+    def Van_print(self):
+        Van.Van_print(self)
+        print("price: ")
+
+   
