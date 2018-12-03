c40335dc88a337c087744b671c781b40da98c657
Bryndís Diego <bryndisdiego@Bryndiss-MacBook.local>
2018-12-03 11:42:40 +0000

    .

--- models/van.py
+++ models/van.py
@@ -1,8 +1,12 @@
 from car import Car 
 
 class van:
-    self.__van = []
+    def __init__(self, price=0):
+        self.__price = price()
 
+    def set_price(self):
+        '''Endurreiknar verð á Van'''
+        return self.__price
 
-   # def get_price(self):
-   #     return self.__price
+    def get_price(self):
+        return self.__price.get_price()
