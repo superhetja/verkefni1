ebb67bb7d58f3c3ffe0b848243682343b1d1230a
Maria Ómarsdottir <mariaomarsdottir@gestur-053.ru.is>
2018-12-04 13:17:03 +0000

    maria

--- repositories/CustomerRepository.py
+++ repositories/CustomerRepository.py
@@ -5,11 +5,11 @@ class CustomerRepository:
    
    def add_costumer(self, costumer):
       with open('./data/costumers.txt', 'a+') as aFile:
-         costumer_file.write(Costumer.__repr__() + '\n')
+         costumers.write(Costumer.__repr__() + '\n')
 
    def get_costumer(self):
       with open("./data/costumers.txt", 'r') as aFile:
-         for line in Costumer_file.readLines():
+         for line in Costumers.readLines():
             Customer = eval(line.strip())
             Customer.append(Customer)
-         reutrn self.__Customer
+         return self.__Customer
