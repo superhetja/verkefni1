4720b22818d1fa49c00468c68bb10cbb1cf1a1ff
Hólmfríður Magnea Hákonardóttir <holmfridur@Holmfriurs-MacBook-Pro.local>
2018-12-05 11:04:38 +0000

    hofi

--- eitthvad.py
+++ eitthvad.py
@@ -1,17 +1,22 @@
-# inní models.video
-def __repr__(self):
-    return "Video('{}','{}','{}')".format(self.__title,self.__length,self.__genre)
+# # inní models.video
+# def __repr__(self):
+#     return "Video('{}','{}','{}')".format(self.__title,self.__length,self.__genre)
 
 
-#inní VideoRepository
-def add_video(self, video):
-    with open('./data/videos.txt', 'a+') as videos_file:
-        videos_file.write(video.__repr__()+ '\n')
+# #inní VideoRepository
+# def add_video(self, video):
+#     with open('./data/videos.txt', 'a+') as videos_file:
+#         videos_file.write(video.__repr__()+ '\n')
 
-def get_videos(self):
-    video = []
-    with open('./data/videos.txt', 'r') as videos_file:
-        for line in videos_file.readlines():
-            video = eval(line.strip())
-            videos.append(video)
-        return videos
+# def get_videos(self):
+#     video = []
+#     with open('./data/videos.txt', 'r') as videos_file:
+#         for line in videos_file.readlines():
+#             video = eval(line.strip())
+#             videos.append(video)
+#         return videos
+
+name = 'Marta Guðbertsdóttir'.split()
+for i in name:
+        if i .isalpha() == False:
+                return False
