b9b9e60972050fc540c98a72aa35f5b4eb36ec93
Maria Ómarsdottir <mariaomarsdottir@gestur-034.ru.is>
2018-12-05 16:09:16 +0000

    breytti sma

--- services/OrderService.py
+++ services/OrderService.py
@@ -17,83 +17,71 @@ class OrderService(self):
     #    payment = is_valid_payment(order)
     #    card_number = is_valid_card_number(order)
         return True
-
 #búið
     def is_valid_date(self, date):
-        errorpromt = 'Rangur innsláttur\nSláðu inn aftur.'
+        errorprompt = 'Rangur innsláttur\nSláðu inn einungis tölustafi.'
             try:
                 date = int(date)
                 if 7 < date < 1:
-                    errorpromt = 'Röng dagsetning.'
+                    errorprompt = 'Ógild dagsetning dagsetning.'
                     raise ValueError
                 return None
             except ValueError:
                 return errorprompt     
-        #     self.__addorder_ui.get_another_input(errorpromt,inputprompt)
-
 #búið
-    def is_valid_group(self, group)
+    def is_valid_group(self, group):
+        errorprompt = 'Ógildur hópur\nSláðu inn tölustaf.'
         try:
             group = int(group)
             if 7 < group <1:
-                errorpromt = 'Rangur innsláttur!'
+                errorprompt = 'Sláðu inn gildan hóp.'
                 raise ValueError
             return None
         except ValueError:
-            pass
-     #  self.__addorder_ui.get_another_input(errorpromt,inputprompt)
-    
+            return errorprompt   
 #búið
     def is_valid_brand(self, brand):
-        errorprompt = 'Ekki rétt tegund.'
+        errorprompt = 'Ekki rétt tegund\nSláðu inn gilda tegund.'
         brand = brand.capitalize()
         if brand in self.VALID_BRANDS:
             return None
         else:
             return errorprompt
-         #   self.__addorder_ui.get_another_input(errorprompt,inputprompt)
-
 #eftir    
     def ia_valid_user_choice(self, user_choice):
-        errorprompt = 'Rangur innsláttur. '
+        errorprompt = 'Rangur innsláttur.'
         try:
             user_choice = user_choice.capitalize()
             if user_choice in self.VALID_BRANDS:
                 break
-       # self.__addorder_ui.get_another_input(errorprompt,inputprompt)
-
-
 #búið ????
     def is_valid_costumer(self, costumer):
-        errorprompt = 'Rangur viðskiptavinur! '
+        errorprompt = 'Rangur viðskiptavinur!'
         for i in costumer:
             if i in costumer:
                 if i.isalpha() == False:
                     return False
                 else:
                     return True
-
 #búið 
     def is_valid_payment(self, payment):
         errorprompt = 'Rangur innsláttur! '
         payment.capitalize()
-            if payment == K or payment == P:
-                return None
-            else:
-                return errorprompt
-
+        if payment == 'K' or payment == 'P':
+            return None
+        else:
+            return errorprompt
 #búið
     def is_valid_card_number(self, card_number):
-        errorprompt = 'Rangt kortanúmer'
-            try:
-               card_number = int(card_number)
-                if len(str(card_number)) != 16 : 
-                    errorpromt = 'Rangt kortanúmer reyndu aftur.'
-                    raise ValueError
-                return None
-            except ValueError:
-                return errorprompt
-                # self.__addorder_ui.get_another_input(errorprompt, inputprompt)
+        errorprompt = 'Rangt kortanúmer\Sláðu inn eingöngu tölustafi.'
+        try:
+            card_number = int(card_number)
+            if len(str(card_number)) != 16 : 
+                errorpromt = 'Of margir tölustafir\nSláðu inn réttan fjölda tölustafa.'
+                raise ValueError
+            return None
+        except ValueError:
+            return errorprompt
 # vantar info ; #vantar 
     def is_valid_info(self, info):
         errorprompt = 'Rangar upplýsingar '
