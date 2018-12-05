b57e5047455c238b495785f46da62a4ff6e21a63
Bryndís Diego <bryndisdiego@Bryndiss-MacBook.local>
2018-12-05 09:53:42 +0000

    bd

--- services/OrderService.py
+++ services/OrderService.py
@@ -17,17 +17,45 @@ class OrderService(self):
         payment = check_payment(order)
         card_number = check_card_number(order)
 
-    def is_valid_date(self, date)
-        if date == int(date) and len (date) == 6:
-            return True
-        else:
-            return False
+        return True
 
-    def is_valid_group(self, group)
-        if group == int(group) 
-            return True
-        else:
-            return False
+    def check_date(self, order)
+        date = date.get_date(date)
+        inputprompt = 'Sláðu inn úttektardag: '
+        while True:
+            errorpromt = 'Rangur innsláttur/date\nSláðu inn aftur.'
+            try:
+                date = int(date)
+                if 7 < date < 1:
+                    errorpromt = 'Röng dagsetning.'
+                    raise ValueError
+                break
+            except ValueError
+                errorpromt = 'Rangur innsláttur/date\nSláðu inn aftur.'
+                self.__addorder_ui.get_another_input(errorpromt,inputprompt)
+
+    def check_group(self, order)
+        group = group.get_group(group)
+        inputprompt = 'Bílaflokkur: '
+        errorpromt = 'Rangur bílaflokkur '
+        while True:
+
+        #VALNTARRRRR !!!!
+        self.__addorder_ui.get_another_input(errorpromt,inputprompt)
     
-    def is_valid_brand(self, brand)
-        if brand == 
+    def check_brand(self, order):
+        brand = Car.get_brand(brand)
+        inputprompt = 'Tegund: '
+        errorprompt = 'Ekki rétt tegund.'
+        while True:
+            brand = brand.capitalize()
+            if brand in self.VALID_BRANDS:
+                break
+            self.__addcar_ui.get_another_input(errorprompt,inputprompt)
+    
+    def check_user_choice(self, order):
+        user_choice = user_choice.get_user_choice(user_choice)
+        inputprompt = 'Valinn bíll: '
+        errorprompt = 'Rangur innsláttur. '
+        while True:
+            
