33ccf33568c995f4e15fd13947cbbce8c9b5acbd
Hólmfríður Magnea Hákonardóttir <holmfridur@Holmfriurs-MacBook-Pro.local>
2018-12-07 13:28:55 +0000

    hofi

--- eitthvad.py
+++ eitthvad.py
@@ -1,7 +1,13 @@
-a = 'Gunna'
-b = 'Gu'
-if b in a:
-    print(a)
+a = '04.122005'
+if '.' in a:
+    seperator = '.'
+elif '/' in a:
+    seperator = '/'
+elif '-' in a:
+    seperator = '-'
+day,month,year = a.split(seperator)
 
-
+print(day)
+print(month)
+print(year)
 
