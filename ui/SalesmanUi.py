55c98c95ab456c8265898b88d3c75b30b8bd2911
sara <sarae17@ru.is>
2018-12-07 00:30:23 +0000

    sara

--- ui/SalesmanUi.py
+++ ui/SalesmanUi.py
@@ -3,12 +3,21 @@ from ui.HeaderUi import Header
 from models.Clear import Clear
 from models.Color import Color
 from ui.InputUi import InputUi
+from ui.OrderUi import OrderUi
+from ui.ShowCarsUi import ShowCars
+from ui.ShowCustomerUi import ShowCustomer
+from CustomerUi import Customer
 
 class Salesman: 
     def __init__(self):
         self.__car_service = CarService()
         self.__header = Header()
         self.__input_ui = InputUi()
+        self.__order=OrderUi()
+        self.__showcar=ShowCars()
+        self.__showcustomer=ShowCustomerUi()
+        self.__customer=CustomerUi()
+        
 
     def main_menu(self):
         print(self.__header)
@@ -28,4 +37,23 @@ class Salesman:
         print("7. Skoða viðskiptavini ")
 
         action = self.__input_ui.get_number_between(1,7)
+        if action=='1':
+            self.__order.set_order()
+        elif action=='2':
+            pass
+        elif action=='3':
+            pass
+        elif action=='4':
+            pass
+        elif action =='5':
+            self.__showcar.show_cars_menu()
+        elif action =='6':
+            self.__customer.new_customer()
+        elif action =='7':
+            self.__showcustomer.show_customer_menu()
+
+
+            
+
+
 
