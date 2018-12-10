54f62c8f92fe74012dcd4d8fcc26d3d32857f14c
Maria Ómarsdottir <mariaomarsdottir@gestur-083.ru.is>
2018-12-10 12:59:37 +0000

    maria

--- ui/CreateEmployeeUi.py
+++ ui/CreateEmployeeUi.py
@@ -3,25 +3,26 @@ from ui.HeaderUi import Header
 from services.EmployeeService import EmployeeService
 from models.Color import Color
 from models.Clear import Clear
-from ui.InputUi import InputUi
+from ui.Ui import Ui
 
-class CreateEmployeeUi:
+class CreateEmployee:
     def __init__(self):
+        Ui.__init__(self)
         self.__header = Header()
         self.__service = EmployeeService()
         self.__clear = Clear()
-        self.__input = InputUi()
+        #self.__input = InputUi()
         
     def create_empoyee(self):
         self.__clear.clear_screen()
         print(self.__header)
         print(Color.BOLD + 'Nýskráning starfsmanna'+ Color.END)
         print('Sláðu inn upplýsingar um starfsmann')
-        name = self.__input.get_string(self.__input.NAMEPROMPT)
+        name = self.get_string(self.NAMEPROMPT)
         password = input(str("Lykilorð: "))
-        ssn = self.__input.get_number_length(self.__input.SSNPROMPT, 10)
-        email = self.__input.get_email()
-        admin = self.__input.get_admin(self.__input.ADMINPROMPT)
+        ssn = self.get_number_length(self.SSNPROMPT, 10)
+        email = self.get_email()
+        admin = self.get_admin(self.ADMINPROMPT)
         
         new_employee = Employee(name, ssn, password, email, admin)
         self.__service.add_employee(new_employee)
--- ui/CustomerUi.py
+++ ui/CustomerUi.py
@@ -6,17 +6,18 @@ from ui.Ui import Ui
 
 class CustomerUi:
     def __init__(self):
+        Ui.__init__(self)
         self.__service = CustomerService()
         self.__header = Header()
-        self.__input = Ui()
+        #self.__input = Ui()
 
     def new_customer(self):
         print(self.__header)
         print(Color.BOLD + "Nýskráning viðskiptavina" + Color.END)
         print("Sláðu inn upplýsingar um viðskiptavin:")
-        name = self.__input.get_string(self.__input.NAMEPROMPT)
-        ssn = self.__input.get_number_length(self.__input.SSNPROMPT, 10)
-        email = self.__input.get_email()
-        phonenr = self.__input.get_number_length(self.__input.PHONEPROMPT,7)
+        name = self.get_string(self.NAMEPROMPT)
+        ssn = self.get_number_length(self.SSNPROMPT, 10)
+        email = self.get_email()
+        phonenr = self.get_number_length(self.PHONEPROMPT,7)
         NewCustomer = Customer(name, ssn, email, phonenr)
         self.__service.add_customer(NewCustomer)
