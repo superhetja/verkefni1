f111cfb88e633e39eadc2713363a94bc655c5cac
Bryndís Diego <bryndisdiego@Bryndiss-MacBook.local>
2018-12-10 14:24:17 +0000

    bd

---
+++ ui/ShowEmployeeUi.py
@@ -0,0 +1,103 @@
+from services.EmployeeService import EmployeeService
+from services.CheckInputService import CheckInput
+from models.Customer import Customer
+from ui.HeaderUi import Header
+#from models.Clear import Clear
+from models.Color import Color
+from ui.Ui import Ui
+
+
+class ShowEmployee(Ui):
+    def __init__(self):
+        Ui.__init__(self)
+        self.__service = EmployeeService()
+        self.__header = Header()
+        self.__color = Color()
+        self.__check_input = CheckInput()
+    #    self= InputUi()
+
+    def print_menu(self):
+        print(Color.BOLD+'Fletta upp starfsmanni'+Color.END)
+        print('1. Fletta upp starfsmanni')
+        print('2. Birtna alla starfsmenn')
+
+    def show_employee_main(self):
+        #eftir .........
+        '''Aðal fallið til að byrja klassan'''
+        self.clear_screen()
+        self.print_menu()
+        action = self.get_number_between(1,2) #self.get_number_between(1,5)
+        if action == '1':
+            self.search_customer()
+        else:
+            self.print_all_customers()
+    #---------------------------------------------
+
+
+    def search_employee(self):
+        search = input('Sláðu inn leitarstreng: ')
+        employee = self.__service.get_matches(search)
+        self.print_list(employee)
+        if len(employee) == 0:
+            self.get_more()
+        else:
+            self.choice_employeer(employee)
+
+    def print_all_employees(self):
+        employee = self.__serfvice.get_full_centent()
+        self.print_lisr(employee)
+        if len(employee) == 0:
+            self.get_more()
+        else:
+            self.choice_employee(employee)
+
+    # def get_more(self):
+    #     letter = self.get_letter(self.MOREPROMPT,['j','n'])
+    #     if letter == 'j':
+    #         self.show_employee_main()
+    #     else:
+    #         pass
+
+    def choice_employee(self, a_list):
+        choice = self.get_number_between(1,len(a_list)+1)
+        chosen_employee = a_list[int(choice)-1]
+        print(chosen_employee)
+        print('1. Breyta starfsmanni')
+        print('2. Eyða starfsmanni')
+        print('3. Fletta upp öðrum starfsmanni')
+        print('4. Fara til baka á valmynd')
+        action Ö self.get_number_between(1,4)
+        if action == '1':
+            self.__service.remove_instance(chosen_employee)
+            print('Starfsmanni eytt')
+            self.get_more()
+        elif action == '2':
+            self.__change_employee(chosen_employee)
+        elif action == '3':
+            self.show_employee_main()
+        else:
+            pass
+    
+    def __change_employee(self, employee):
+        print('Hverju viltu breyta?')
+        print('1. Nafni')
+        print('2. Kenntölu')
+        print('3. Netfangi')
+        print('4. Símanúmeri')
+        action = self.get_number_between(1,4)
+        name = employee.get_name()
+        ssn = employee.get_ssn()
+        email = employee.get_email()
+        phonenr = employee.get_phoneNr()
+        if action == '1':
+            name = self.get_string(self.NAMEPROMPT)
+        elif action == '2':
+            ssn = self.get_number_length(self.SSNPROMPT,10)
+        elif action == '3':
+            email = self.get_email()
+        else:
+            phonenr = self.get_number_length(self.PHONEPROMPT,7)
+        new_employee= employee(name,ssn,email,phonenr)
+        self.__service.change_instance(employee, new_employee)
+        print('Breytingu lokið.')
+        self.get_more()
