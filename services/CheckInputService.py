f648b680647202c2b92e158a8d41ed9773862f7b
Bryndís Diego <bryndisdiego@Bryndiss-MacBook.local>
2018-12-12 12:43:42 +0000

    bd

--- services/CheckInputService.py
+++ services/CheckInputService.py
@@ -1,5 +1,5 @@
 import datetime
-
+from models.Color import Color
 class CheckInput:
 
     #ACTION_CHOICES=[1,2,3,4,5,6,7,8]
@@ -12,11 +12,11 @@ class CheckInput:
 
     def is_valid_number_between(self, num, lower, higher):
         '''Tekur inn breytu og bil og segir til um hvort breytan se heiltala og se a bilinu'''
-        errorprompt = 'Incorrect input\nEnter numbers only'
+        errorprompt = Color.RED+'Incorrect input\nEnter numbers only'+Color.END
         try:
             num = int(num)
             if num > higher or num < lower:
-                errorprompt = 'Input out of range'
+                errorprompt = Color.RED+'Input out of range'+Color.END
                 raise ValueError
             return None
         except ValueError:
@@ -24,7 +24,7 @@ class CheckInput:
             
     def is_valid_email(self, email):
         '''Tekur inn netfang og athugar hvort thad se gilt'''
-        errorprompt = 'Incorrect email\nEnter a valid email address'
+        errorprompt = Color.RED+'Incorrect email\nEnter a valid email address'+Color.END
         try:
             atsymbol = email.index('@')
             email.index('.', atsymbol)
@@ -34,7 +34,7 @@ class CheckInput:
 
     def is_valid_number(self,num):
         '''Tekur inn breytu og athugar hvort ad hun se heiltala'''
-        errorprompt = 'Incorrect input\nEnter numbers only'
+        errorprompt = Color.RED+'Incorrect input\nEnter numbers only'+Color.END
         try:
             int(num)
             return None
@@ -43,14 +43,14 @@ class CheckInput:
 
     def is_valid_letter(self, letter, valid_letters):
         '''Tekur inn staf og lista af gildum stofum og athugar hvort stafurinn se gildur stafur'''
-        errorprompt = 'Incorrect input'
+        errorprompt = Color.RED+'Incorrect input'+Color.END
         if letter in valid_letters:
             return None
         return errorprompt
 
     def is_string(self, string):
         '''Tekur inn streng og athugar hvort ad allt i strengnum se stafur'''
-        errorprompt = 'Incorrect input\nEnter letters only'
+        errorprompt = Color.RED+'Incorrect input\nEnter letters only'+Color.END
         string = string.split()
         for i in string:
             if i.isalpha() == False:
@@ -59,11 +59,11 @@ class CheckInput:
 
     def is_valid_number_length(self, num, length):
         '''Tekur inn breytu og lengd a streng og athugar hvort að breytan se heiltala og se rett legnd'''
-        errorprompt = 'Incorrect input\nEnter numbers only'
+        errorprompt = Color.RED+'Incorrect input\nEnter numbers only'+Color.END
         try:
             num = int(num)
             if len(str(num)) != length:
-                errorprompt = 'Incorrect number of digits'
+                errorprompt = Color.RED+'Incorrect number of digits'+Color.END
                 raise ValueError
             return None
         except ValueError:
@@ -71,7 +71,7 @@ class CheckInput:
 
     def is_valid_date(self, date):
         '''Tekur inn dagsetningu og athugar hvort ad hun se gild'''
-        errorprompt = 'Invalid date'
+        errorprompt = Color.RED+'Invalid date'+Color.END
         try:
             if '.' in date:
                 seperator = '.'
@@ -86,7 +86,7 @@ class CheckInput:
             return(errorprompt)
 
     def is_carnum(self, carnum):
-        errorprompt = 'Incorrect input'
+        errorprompt = Color.RED+'Incorrect input'+Color.END
         if len(carnum) == 5 and carnum.isalnum():
             return None
         return errorprompt
