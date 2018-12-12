19e461a926faa103a837377363f7650310c444af
sara <sarae17@ru.is>
2018-12-12 10:47:06 +0000

    sara

--- models/Order.py
+++ models/Order.py
@@ -2,8 +2,10 @@ from datetime import date
 from models.Price import Price
 
 class Order: 
+    """Klasi fyrir pöntunina"""
 
     def __init__(self, date1, date2, group, car,extra_insurance, customer, payment, card_number='',returned = False):
+        """Fall með privit breytum"""
         self.__date1 = date1
         self.__date2 = date2
         self.__car = car
@@ -16,25 +18,33 @@ class Order:
         self.__price = self.calculate_price(group)
 
     def __str__(self):
+        """Prentar út upplýsingar um tíman, bílin, trygginguna og fl."""
         return "Time period: {} - {}\nCar: {}\nExtra Insurance: {}\nPrice: {}\nCustomer: {}\nPayment: {}\nCard number: {}\n Status: {}".format(self.__date1, self.__date2, self.__car, self.__price, self.__customer, self.__payment, self.__card_number, self.__returned)
         
     def __repr__(self):
+        """Prentar út upplýsingarnar um pöntunina"""
         return "Order('{}','{}','{}','{}','{}','{}','{}','{}','{}')".format(self.__date1,
         self.__date2,self.__group,self.__car,self.__extra_insurance,self.__customer,self.__payment,self.__card_number,
         self.__returned)
 
     def get_date1(self):
+        """Nær í dagsetningu sem bíllinn er leigður"""
         return self.__date1
     
     def get_date2(self):
+        """Nær í dagsetninguna sem bíllinn er skilaður"""
         return self.__date2
 
     def calculate_price(self, group):
+        """Reiknar út hvað það kostar að leigja bíl í einn dag 
+        og margfaldar það með tímann sem hann er leigður,skilar útkomunni"""
         price_per_day = self.get_price_per_day(group)
         price_all_period = price_per_day * self.get_time_period()
         return price_all_period
     
     def get_time_period(self):
+        """Nær í tímann sem bíllinn er leigður með því að setja það í lista, breyta í heiltölu
+        og draga dagsetningu 1 við dagsetningu 2. skilar tímanum sem hann er leigður"""
         year1, month1, day1 = self.__date1.split('-')
         year2, month2, day2 = self.__date2.split('-')
         date1 = date(int(year1), int(month1), int(day1))
@@ -43,6 +53,7 @@ class Order:
         return time_period
 
     def get_price_per_day(self, group):
+        '''Skilar greiðslu á hvað kostar að leiga í einn dag'''
         price_per_day = Price.price_dict[group][Price.PRICE]
         return price_per_day
 
